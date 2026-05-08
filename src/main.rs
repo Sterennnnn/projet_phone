99c9a8898b47b5e29e85f6773ca3d80ec1874202
arthur lagneau <arthurlagneau@gmail.com>
2026-05-08 22:30:35 +0200

    feat: modifier main

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,13 @@
 use phone_trie::{lire_contacts, generer_plantuml};
 use phone_trie::trie::Trie;
 
- 
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let contacts = lire_contacts("data/contacts.json")?;
-
- 
+    let contacts = lire_contacts("data/contacts.json")?; 
 
     let mut trie = Trie::new();
     for contact in &contacts {
         trie.insert(&contact.phone, &contact.name);
     }
-
- 
 
     let puml = trie.to_plantuml();
     generer_plantuml("graph/contacts.puml", &puml)?;
